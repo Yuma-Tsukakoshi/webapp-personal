413b857c2a688b2dba4eaf9f0f03415d56d63176
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2022-10-01 21:34:50 +0900

    slidedown-menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,14 @@
 $('.exam-list').hide();
 $('.exam-slidedown').mouseover(function() {
   $('.exam-list').slideDown();
-}).mouseout(function(){
+  });
+$('main').mouseover(function() {
   $('.exam-list').slideUp();
-});
+  });
 $('.machine-list').hide();
 $('.machine-slidedown').mouseover(function() {
   $('.machine-list').slideDown();
-}).mouseout(function(){
+})
+$('main').mouseover(function() {
   $('.machine-list').slideUp();
-});
-// 改善点：mouseout時にli要素全体にかかっていない
-//slideDownで要素幅よりも大きくして出して固定したい
-//周りも一緒に動いちゃう→headerの高さが決まっちゃってる
-//
+  });
