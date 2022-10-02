03a91572257c67e898cc723ffeb830a90e48e769
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2022-10-02 10:43:27 +0900

    modal open&close

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,1 +1,9 @@
 'use strict';
+
+$('#js-openModal').click(function(){
+  $('.overlay').addClass('openModal')
+  $('#js-buckModal').css("display","none")
+})
+$('#js-closeModal').click(function(){
+  $('.overlay').removeClass('openModal')
+})
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,12 @@ $('.exam-slidedown').mouseover(function() {
   $('.exam-list').slideDown();
   });
 $('main').mouseover(function() {
-  $('.exam-list').slideUp();
+  $('.exam-list').slideUp(200);
   });
 $('.machine-list').hide();
 $('.machine-slidedown').mouseover(function() {
   $('.machine-list').slideDown();
 })
 $('main').mouseover(function() {
-  $('.machine-list').slideUp();
+  $('.machine-list').slideUp(200);
   });
